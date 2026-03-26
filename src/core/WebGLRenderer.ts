8d84a4ec4426e91e15a1e752d1bc47acf5be67e9
Alpinist <alpinistnex@outlook.com>
2026-03-26 04:29:13 -0400

    fix some bugs

--- src/core/WebGLRenderer.ts
+++ src/core/WebGLRenderer.ts
@@ -66,12 +66,17 @@ function createTexture(gl: WebGL2RenderingContext, width: number, height: number
   if (!texture) {
     throw new Error('Unable to create texture.');
   }
+
+  const isMaskTexture = internalFormat === gl.R8;
+  const format = isMaskTexture ? gl.RED : gl.RGBA;
+  const type = gl.UNSIGNED_BYTE;
+
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+  gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, null);
   return texture;
 }
 
